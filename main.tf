f3a83dd5379b6071258d2490aa7241d8c648b6fe
Shahbaz <Shahbaz@gmail.com>
2026-04-21 13:57:30 +0530

    main.tf

---
+++ main.tf
@@ -0,0 +1,182 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.68.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "chintu"          # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "mintu2512"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "chintu12"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "prod.terraform.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# -------------------------------
+# Resource Group
+# -------------------------------
+resource "azurerm_resource_group" "rg" {
+  name     = "myResourceGroup"
+  location = "Central India"
+}
+
+# -------------------------------
+# Virtual Network
+# -------------------------------
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myVnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+  
+}
+
+# -------------------------------
+# Subnet
+# -------------------------------
+resource "azurerm_subnet" "subnet" {
+  name                 = "mySubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# -------------------------------
+# Public IP (Standard - REQUIRED)
+# -------------------------------
+resource "azurerm_public_ip" "pip" {
+  name                = "myPublicIP"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  allocation_method = "Static"
+  sku               = "Standard"
+  
+}
+
+# -------------------------------
+# Network Security Group
+# -------------------------------
+resource "azurerm_network_security_group" "nsg" {
+  name                = "myNSG"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  # SSH (22)
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  # HTTP (80)
+  security_rule {
+    name                       = "Allow-HTTP"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# -------------------------------
+# Network Interface
+# -------------------------------
+resource "azurerm_network_interface" "nic" {
+  name                = "myNIC"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+# -------------------------------
+# NSG Association
+# -------------------------------
+resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# -------------------------------
+# Linux VM + Auto Nginx Setup
+# -------------------------------
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "myLinuxVM"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_D2s_v3"
+  admin_username      = "azureuser"
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file(pathexpand("~/.ssh/id_rsa.pub"))
+  }
+
+  os_disk {
+    name                 = "myOsDisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  computer_name = "myvm"
+  disable_password_authentication = true
+
+  # 🔥 AUTO INSTALL NGINX + WEB PAGE
+  custom_data = base64encode(<<EOF
+#!/bin/bash
+
+apt update -y
+apt install nginx -y
+
+systemctl enable nginx
+systemctl start nginx
+
+echo "🚀 Shabaz Deepak Honey Indra 🚀" > /var/www/html/index.html
+
+systemctl restart nginx
+EOF
+  )
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+# -------------------------------
+# OUTPUT PUBLIC IP
+# -------------------------------
+output "public_ip" {
+  value = azurerm_public_ip.pip.ip_address
+}
